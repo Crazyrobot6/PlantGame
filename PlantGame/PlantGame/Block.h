edac89e5136edf0bf72eb688d49643bc2f01d46b
Joshua Vorick <josh.vorick@gmail.com>
2013-07-12 12:28:43 -0500

    Added to main, trimmed Block
    removed above, below, east, etc

--- PlantGame/PlantGame/Block.h
+++ PlantGame/PlantGame/Block.h
@@ -5,38 +5,29 @@
 #include <vector>
 #include <string>
 
-enum blockIDS {AIR, SOIL1, SOIL2, SOIL3, SOIL4};
+enum blockIDS {SOIL1, SOIL2, SOIL3, SOIL4}; //AIR is NULL pointer, see GameMap
+const int NUM_SOIL_TYPES = 4;
+const int MAX_PLAYERS = 8;
 
 class Block
 {
 private:
 	int blockID;				//Pretty obvious -- an ID describing what type of block it is
-	ALLEGRO_BITMAP* image;				//File path for the corresponding bitmap image
+	ALLEGRO_BITMAP* images[NUM_SOIL_TYPES];		//File path for the corresponding bitmap image
 	int mineralsAvailable;		//How many minerals are currently available in the soil block
 	int mineralsGivenPerTurn;	//How many total minerals the block gives per turn -- to be divided up among players
 	int mineralsReplenishedPerTurn;	//How many minerals are given to the block after each turn cycle
 	int waterAvailable;		//How much water is currently available in the soil block
 	int waterGivenPerTurn;	//How much total water the block gives per turn -- to be divided up among players
 	int waterReplenishedPerTurn;	//How much water is given to the block after each turn cycle
-	std::vector<double> percentControlledByPlayer;	//Array of doubles giving the percent owned by each player as a decimal
-
-	Block* below;	//Pointers for adjacent blocks; will be initialized to null
-	Block* above;
-	Block* north;
-	Block* south;
-	Block* east;
-	Block* west;
+	std::vector<int> numUnitRoots;	//Array of intes giving the number of roots of each unit in the block
+	//Each unit has an id, given to it at creation. Every time a unit is created, an int is added (starting at 0) for every block
 
 public:
 	Block(int id);	//Initializes values minearls/water based on ID passed
 	~Block();	//Does whatever garbage collection is needed
-	Block* getBelow();	//Self-explanatory
-	Block* getAbove();
-	Block* getNorth();
-	Block* getSouth();
-	Block* getEast();
-	Block* getWest();
-	ALLEGRO_BITMAP* getBitmap();	//Returns the bitmap so the block can be drawn
+	ALLEGRO_BITMAP* getBitmap();	//Returns the bitmap so the block can be drawn	//Maybe do a Render(int x, int y) instead of getBitmap()??
+	//either here or in GameMap, we need to add functionality to draw the unit on top of the block right after drawing that block
 	double givePlayerMinerals(int playerNum);	//See function definition for explanation
 	double givePlayerWater(int playerNum);
 	void replenishResources();	//To be called after all players have taken their turns
--- PlantGame/PlantGame/main.cpp
+++ PlantGame/PlantGame/main.cpp
@@ -1,8 +1,11 @@
 //Allegro Headers -- add to as needed
+#include <allegro5\allegro.h>
 #include <allegro5\allegro5.h>
 #include <allegro5\allegro_font.h>
 #include <allegro5\allegro_ttf.h>
 #include <allegro5\allegro_image.h>
+#include <allegro5\allegro_audio.h>
+#include <allegro5\allegro_acodec.h>
 #include <allegro5\allegro_primitives.h>
 
 //Class Headers
@@ -24,36 +27,60 @@
 int main()
 {
 	//Create local variables
-
+	const int FPS = 60;
+	const int NUM_SAMPLES = 8; //number of sounds playing at one time
+	const int WIDTH = 100;
+	const int HEIGHT = 100;
+	int mouseX = 0;
+	int mouseY = 0;
+	bool done = false;
 	//Create Allegro variables
+	ALLEGRO_DISPLAY *display;
+	ALLEGRO_EVENT_QUEUE *event_queue;
+	ALLEGRO_TIMER *timer;
 
 	/*
 	.ini File stuff
 	-Read .ini file -- prob has graphics options and stuff
 	-Process .ini file -- this is where we save the data from the .ini to variables in the program
 	*/
+	if(!al_init()) return -1;
+	al_set_new_display_flags(ALLEGRO_FULLSCREEN);	//changes based on .ini
+	display = al_create_display(WIDTH, HEIGHT);
+	//al_set_window_position(display,0,0);		//if windowed, then sets position to top left corner
+
+	al_install_keyboard();
+	al_install_audio();
+	al_init_acodec_addon();
+	al_init_primitives_addon();
+	al_init_image_addon();
+	al_init_font_addon();
+	al_init_ttf_addon();
+	al_install_mouse();
 
 	/*
 	Initialize Allegro components
-	-Main part of allegro
-	-Initialize display
-	-Initialize fonts
-	-Initialize sound
-	-Whatever other parts of Allegro we need
+	load bitmaps, audio, font, etc
 	*/
 
-	/*
-	Load game and assets
-	-Create ALLEGRO_BITMAP* variables for all bitmaps
-		--We'll hold these in some location
-		--Say Object1 needs Bitmap2. What we do is when we create Object1, we give it a copy of the ALLEGRO_BITMAP pointer
-		--Now we've only loaded the bitmap once, but allow multiple objects to use it to draw
-	-Load in the sound, I haven't read about how that works yet
-	-Maybe load maps at this time? Otherwise we could load them when the game is starting
-	*/
 
+	timer = al_create_timer(1.0/FPS);
+	event_queue = al_create_event_queue();			//create event queue, then register all sources sp ot works
+	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	//al_register_event_source(event_queue, al_get_display_event_source(display));	//For some reason this isn't working...hm...
+	al_register_event_source(event_queue, al_get_mouse_event_source());
+
+	al_reserve_samples(NUM_SAMPLES);	//reserves space for NUM_SAMPLES of samples to play at one time
 	//Set state to start menu
 	
+
+	al_start_timer(timer);	//starts right before game loop does to keep timing happy
+	while(!done)
+	{
+		ALLEGRO_EVENT ev;
+		al_wait_for_event(event_queue, &ev);	//waits for something to happen (timer, keyboard, mouse etc)
+		done = true;		//prevent inf loop
 	/*
 	Game Loop
 		-Read input
@@ -61,6 +88,10 @@ int main()
 		-Update internals
 		-Render output to screen
 	*/
-
+	}
+	al_destroy_timer(timer);
+	al_destroy_event_queue(event_queue);
+	//al_destroy_display(display);
+	al_rest(1);	//Just waits to show that a black box appeared and that this works :D
 	return 0;
 }
